dc22cd1d8cced654f835204678c42c1f9283525a
Paulius Puodziunas <pauliusp@unity3d.com>
2019-04-19 17:23:30 +0300

    Build Android lib on a bokken agent.

--- com.unity.mobile.notifications/publish_package.sh
+++ com.unity.mobile.notifications/publish_package.sh
@@ -22,6 +22,7 @@ brew install jq
 # MANIFEST=$(jq -r ".repository += {\"url\": \"$REPO_URL\", \"revision\": \"$REPO_REV\"}" package.json)
 # echo "$MANIFEST" > package.json
 
+
 curl -u $BIN_USERNAME@unity:$BIN_API_KEY https://packages.unity.com/auth > .npmrc
 sed -i -e 's/npm\/unity\/unity/npm\/unity\/unity-staging/g' .npmrc
 npm publish com.unity.mobile.notifications/upm-ci\~/packages/com.unity.mobile.notifications-1.0.0-preview.15.1.tgz
