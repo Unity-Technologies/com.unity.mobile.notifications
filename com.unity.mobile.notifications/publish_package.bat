3fb39b49030204d1558069f07be90cbeda120297
Paulius Puodziunas <pauliusp@unity3d.com>
2019-06-19 23:21:50 +0300

    Move downloading upm to before_script.

--- com.unity.mobile.notifications/publish_package.bat
+++ com.unity.mobile.notifications/publish_package.bat
@@ -1,6 +1,4 @@
  echo "begin:"
-npm install upm-ci-utils@stable --registry https://api.bintray.com/npm/unity/unity-npm -g
-echo "installed upm-ci"
 echo "publishing package"
 upm-ci package publish
 echo "published package"
