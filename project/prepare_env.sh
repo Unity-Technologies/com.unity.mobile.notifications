5d4e35b3147bce857b33fd765a84de8a27ffe7ca
Paulius Puodziunas <pauliusp@unity3d.com>
2019-06-20 15:45:35 +0300

    Update prepare_env.sh

--- project/prepare_env.sh
+++ project/prepare_env.sh
@@ -17,29 +17,28 @@ export ANDROID_BUILD_TOOLS_VERSION="26.0.2"
     
 # printf "\n SDK Manager license: \n"
 
-# ./usr/bin/expect -c '
-# set timeout -1;
-# spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager --licenses --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128;
-#   expect {
-#     "y/N" { exp_send "y\r" ; exp_continue }
-#     eof
-#   }
-# '
+./usr/bin/expect -c '
+set timeout -1;
+spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager --licenses --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128;
+  expect {
+    "y/N" { exp_send "y\r" ; exp_continue }
+    eof
+  }
+'
 
 ls $ANDROID_HOME
 ls $ANDROID_HOME/tools
 ls $ANDROID_HOME/tools/bin
 
+printf "\n Install Android Build Tool and Libraries: \n" && \
+.$ANDROID_HOME/tools/bin/sdkmanager --update --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128 >/dev/null
 
-# printf "\n Install Android Build Tool and Libraries: \n" && \
-# .$ANDROID_HOME/tools/bin/sdkmanager --update --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128 >/dev/null
+.$ANDROID_HOME/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" \
+    "platforms;android-${ANDROID_VERSION}" \
+    "platform-tools" \
+    --proxy=http \
+    --proxy_host=proxy.bf.unity3d.com\
+    --proxy_port=3128 \
+    >/dev/null
 
-# .$ANDROID_HOME/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" \
-#     "platforms;android-${ANDROID_VERSION}" \
-#     "platform-tools" \
-#     --proxy=http \
-#     --proxy_host=proxy.bf.unity3d.com\
-#     --proxy_port=3128 \
-#     >/dev/null
-
-# printf "\n Finished preparing environment \n"
+printf "\n Finished preparing environment \n"
