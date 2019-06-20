72859ce8a8079b5da7c2cf08e1e4bc8712380957
Paulius Puodziunas <pauliusp@unity3d.com>
2019-06-20 13:49:38 +0300

    Install Android SDK in apt-get.

--- project/prepare_env.sh
+++ project/prepare_env.sh
@@ -4,27 +4,27 @@ set -xeuo pipefail
 mkdir package
 
 export SDK_URL="https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip" 
-export ANDROID_HOME="$HOME/package/android-sdk" 
+export ANDROID_HOME=/usr/lib/android-sdk
 export ANDROID_VERSION="26" 
 export ANDROID_BUILD_TOOLS_VERSION="26.0.2"
 
-printf "\n Download Android SDK: \n"
-mkdir "$ANDROID_HOME" .android \
-    && cd "$ANDROID_HOME" \
-    && curl -o sdk.zip $SDK_URL \
-    && unzip sdk.zip \
-    && rm sdk.zip
+# printf "\n Download Android SDK: \n"
+# mkdir "$ANDROID_HOME" .android \
+#     && cd "$ANDROID_HOME" \
+#     && curl -o sdk.zip $SDK_URL \
+#     && unzip sdk.zip \
+#     && rm sdk.zip
     
-printf "\n SDK Manager license: \n"
+# printf "\n SDK Manager license: \n"
 
-./usr/bin/expect -c '
-set timeout -1;
-spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager --licenses --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128;
-  expect {
-    "y/N" { exp_send "y\r" ; exp_continue }
-    eof
-  }
-'
+# ./usr/bin/expect -c '
+# set timeout -1;
+# spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager --licenses --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128;
+#   expect {
+#     "y/N" { exp_send "y\r" ; exp_continue }
+#     eof
+#   }
+# '
 
 printf "\n Install Android Build Tool and Libraries: \n" && \
 $ANDROID_HOME/tools/bin/sdkmanager --update --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128 >/dev/null
