df6a9755a2d10a6f1b49e9d45bf25894622e9c73
Paulius Puodziunas <pauliusp@unity3d.com>
2019-06-20 16:34:03 +0300

    Update prepare_env.sh

--- project/prepare_env.sh
+++ project/prepare_env.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-mkdir package
-
-export SDK_URL="https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip" 
+export SDK_URL="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" 
 export ANDROID_HOME=~/android-sdk
 export ANDROID_VERSION="26" 
 export ANDROID_BUILD_TOOLS_VERSION="26.0.2"
 
 # printf "\n Download Android SDK: \n"
-mkdir "$ANDROID_HOME" .android \
-    && cd "$ANDROID_HOME" \
-    && curl -o sdk.zip $SDK_URL \
-    && unzip sdk.zip \
-    && rm sdk.zip
+
+#mkdir "$ANDROID_HOME"  \
+ #   && cd "$ANDROID_HOME" \
+  #  && curl -o sdk.zip $SDK_URL \
+   # && unzip sdk.zip \
+    #&& rm sdk.zip
     
 # printf "\n SDK Manager license: \n"
 
@@ -26,21 +25,18 @@ mkdir "$ANDROID_HOME" .android \
 #   }
 # '
 
-yes | .$ANDROID_HOME/tools/bin/sdkmanager --licenses
-
-ls $ANDROID_HOME
-ls $ANDROID_HOME/tools
-ls $ANDROID_HOME/tools/bin
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 
 printf "\n Install Android Build Tool and Libraries: \n" && \
-.$ANDROID_HOME/tools/bin/sdkmanager --update --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128 >/dev/null
+$ANDROID_HOME/tools/bin/sdkmanager --update >/dev/null
+#$ANDROID_HOME/tools/bin/sdkmanager --update --proxy=http --proxy_host=proxy.bf.unity3d.com --proxy_port=3128 >/dev/null
 
-.$ANDROID_HOME/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" \
+$ANDROID_HOME/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" \
     "platforms;android-${ANDROID_VERSION}" \
-    "platform-tools" \
-    --proxy=http \
-    --proxy_host=proxy.bf.unity3d.com\
-    --proxy_port=3128 \
-    >/dev/null
+    "platform-tools" #\
+  #  --proxy=http \
+  #  --proxy_host=proxy.bf.unity3d.com\
+  #  --proxy_port=3128 \
+  #  >/dev/null
 
 printf "\n Finished preparing environment \n"
