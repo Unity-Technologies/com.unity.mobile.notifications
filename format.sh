b624f38961c11cb8ff2f84b2af30e1734dd1d7a8
Vincent Zhang <vincent@unity3d.com>
2020-02-20 11:35:20 +0800

    Add ./TestProjects/NotificationSamples/Assets to formatter.

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-perl ~/unity-meta/Tools/Format/format.pl ./com.unity.mobile.notifications/Editor ./com.unity.mobile.notifications/Runtime ./com.unity.mobile.notifications/Tests
+perl ~/unity-meta/Tools/Format/format.pl ./com.unity.mobile.notifications/Editor ./com.unity.mobile.notifications/Runtime ./com.unity.mobile.notifications/Tests ./TestProjects/NotificationSamples/Assets
