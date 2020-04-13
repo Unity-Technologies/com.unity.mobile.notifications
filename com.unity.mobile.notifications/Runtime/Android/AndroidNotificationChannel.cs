7d5a7f9cd9251568f7a3ab9735aebb858396ca7c
Vincent Zhang <vincent@unity3d.com>
2020-04-13 23:18:04 +0800

    Fixed issue #35 by setting Private to 0, and Public to -1000.

--- com.unity.mobile.notifications/Runtime/Android/AndroidNotificationChannel.cs
+++ com.unity.mobile.notifications/Runtime/Android/AndroidNotificationChannel.cs
@@ -46,12 +46,12 @@ namespace Unity.Notifications.Android
         /// <summary>
         /// Show this notification on all lock screens, but conceal sensitive or private information on secure lock screens.
         /// </summary>
-        Private = -1000,
+        Private = 0,
 
         /// <summary>
         /// Show this notification in its entirety on the lock screen.
         /// </summary>
-        Public = 1,
+        Public = -1000,
     }
 
     public struct AndroidNotificationChannel
