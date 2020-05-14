f491fb45f745c573ef9bf6659cd246331f5b756b
Vincent Zhang <vincent@unity3d.com>
2020-05-14 21:50:40 +0800

    Early return.

--- com.unity.mobile.notifications/Editor/iOSNotificationPostProcessor.cs
+++ com.unity.mobile.notifications/Editor/iOSNotificationPostProcessor.cs
@@ -14,123 +14,123 @@ public class iOSNotificationPostProcessor : MonoBehaviour
     [PostProcessBuild]
     public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
     {
-        if (buildTarget == BuildTarget.iOS)
+        if (buildTarget != BuildTarget.iOS)
+            return;
+
+        var projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+        var proj = new PBXProject();
+        proj.ReadFromString(File.ReadAllText(projPath));
+
+        string mainTarget;
+        string unityFrameworkTarget;
+
+        var unityMainTargetGuidMethod = proj.GetType().GetMethod("GetUnityMainTargetGuid");
+        var unityFrameworkTargetGuidMethod = proj.GetType().GetMethod("GetUnityFrameworkTargetGuid");
+
+        if (unityMainTargetGuidMethod != null && unityFrameworkTargetGuidMethod != null)
         {
-            var projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-
-            var proj = new PBXProject();
-            proj.ReadFromString(File.ReadAllText(projPath));
-
-            string mainTarget;
-            string unityFrameworkTarget;
-
-            var unityMainTargetGuidMethod = proj.GetType().GetMethod("GetUnityMainTargetGuid");
-            var unityFrameworkTargetGuidMethod = proj.GetType().GetMethod("GetUnityFrameworkTargetGuid");
-
-            if (unityMainTargetGuidMethod != null && unityFrameworkTargetGuidMethod != null)
-            {
-                mainTarget = (string)unityMainTargetGuidMethod.Invoke(proj, null);
-                unityFrameworkTarget = (string)unityFrameworkTargetGuidMethod.Invoke(proj, null);
-            }
-            else
-            {
-                mainTarget = proj.TargetGuidByName("Unity-iPhone");
-                unityFrameworkTarget = mainTarget;
-            }
-
-            var settings = NotificationSettingsManager.Initialize().iOSNotificationSettingsFlat;
-
-            var addPushNotificationCapability = (bool)settings.Find(i => i.Key == "UnityAddRemoteNotificationCapability").Value;
-
-            var needLocationFramework = (bool)settings.Find(i => i.Key == "UnityUseLocationNotificationTrigger").Value;
-
-            proj.AddFrameworkToProject(unityFrameworkTarget, "UserNotifications.framework", true);
-
-            if (needLocationFramework)
-                proj.AddFrameworkToProject(unityFrameworkTarget, "CoreLocation.framework", false);
-
-            File.WriteAllText(projPath, proj.WriteToString());
-
-            if (addPushNotificationCapability)
-            {
-                var useReleaseAPSEnvSetting = settings.Find(i => i.Key == "UnityUseAPSReleaseEnvironment");
-                var useReleaseAPSEnv = false;
-
-                if (useReleaseAPSEnvSetting != null)
-                    useReleaseAPSEnv = (bool)useReleaseAPSEnvSetting.Value;
-
-                var entitlementsFileName = proj.GetBuildPropertyForAnyConfig(mainTarget, "CODE_SIGN_ENTITLEMENTS");
-                if (entitlementsFileName == null)
-                {
-                    var bundleIdentifier = PlayerSettings.GetApplicationIdentifier(BuildTargetGroup.iOS);
-                    entitlementsFileName = string.Format("{0}.entitlements",
-                        bundleIdentifier.Substring(bundleIdentifier.LastIndexOf(".") + 1));
-                }
-
-                var pbxPath = PBXProject.GetPBXProjectPath(path);
-                var capManager = new ProjectCapabilityManager(pbxPath, entitlementsFileName, "Unity-iPhone");
-                capManager.AddPushNotifications(!useReleaseAPSEnv);
-                capManager.WriteToFile();
-            }
-
-            // Get plist
-            var plistPath = path + "/Info.plist";
-            var plist = new PlistDocument();
-            plist.ReadFromString(File.ReadAllText(plistPath));
-
-            // Get root
-            var rootDict = plist.root;
-
-            var requiredVersion = new Version(8, 0);
-            bool hasMinOSVersion;
-
-            try
-            {
-                var currentVersion = new Version(PlayerSettings.iOS.targetOSVersionString);
-                hasMinOSVersion = currentVersion >= requiredVersion;
-            }
-            catch (Exception)
-            {
-                hasMinOSVersion = false;
-            }
-
-            if (!hasMinOSVersion)
-                Debug.Log("UserNotifications are only available on iOSSettings 10 and above, please make sure that you set a correct `Target minimum iOSSettings Version` in Player Settings.");
-
-            foreach (var setting in settings)
-            {
-                if (setting.Value.GetType() == typeof(bool))
-                    rootDict.SetBoolean(setting.Key, (bool)setting.Value);
-                else if (setting.Value.GetType() == typeof(PresentationOption) || setting.Value.GetType() == typeof(AuthorizationOption))
-                    rootDict.SetInteger(setting.Key, (int)setting.Value);
-            }
-
-            if (addPushNotificationCapability)
-            {
-                PlistElementArray currentBacgkgroundModes = (PlistElementArray)rootDict["UIBackgroundModes"];
-
-                if (currentBacgkgroundModes == null)
-                    currentBacgkgroundModes = rootDict.CreateArray("UIBackgroundModes");
-
-                currentBacgkgroundModes.AddString("remote-notification");
-            }
-
-            // Write to file
-            File.WriteAllText(plistPath, plist.WriteToString());
-
-            //Get Preprocessor.h
-            var preprocessorPath = path + "/Classes/Preprocessor.h";
-            var preprocessor = File.ReadAllText(preprocessorPath);
-
-            if (needLocationFramework)
-            {
-                if (preprocessor.Contains("UNITY_USES_LOCATION"))
-                    preprocessor = preprocessor.Replace("UNITY_USES_LOCATION 0", "UNITY_USES_LOCATION 1");
-            }
-
-            preprocessor = preprocessor.Replace("UNITY_USES_REMOTE_NOTIFICATIONS 0", "UNITY_USES_REMOTE_NOTIFICATIONS 1");
-            File.WriteAllText(preprocessorPath, preprocessor);
+            mainTarget = (string)unityMainTargetGuidMethod.Invoke(proj, null);
+            unityFrameworkTarget = (string)unityFrameworkTargetGuidMethod.Invoke(proj, null);
         }
+        else
+        {
+            mainTarget = proj.TargetGuidByName("Unity-iPhone");
+            unityFrameworkTarget = mainTarget;
+        }
+
+        var settings = NotificationSettingsManager.Initialize().iOSNotificationSettingsFlat;
+
+        var addPushNotificationCapability = (bool)settings.Find(i => i.Key == "UnityAddRemoteNotificationCapability").Value;
+
+        var needLocationFramework = (bool)settings.Find(i => i.Key == "UnityUseLocationNotificationTrigger").Value;
+
+        proj.AddFrameworkToProject(unityFrameworkTarget, "UserNotifications.framework", true);
+
+        if (needLocationFramework)
+            proj.AddFrameworkToProject(unityFrameworkTarget, "CoreLocation.framework", false);
+
+        File.WriteAllText(projPath, proj.WriteToString());
+
+        if (addPushNotificationCapability)
+        {
+            var useReleaseAPSEnvSetting = settings.Find(i => i.Key == "UnityUseAPSReleaseEnvironment");
+            var useReleaseAPSEnv = false;
+
+            if (useReleaseAPSEnvSetting != null)
+                useReleaseAPSEnv = (bool)useReleaseAPSEnvSetting.Value;
+
+            var entitlementsFileName = proj.GetBuildPropertyForAnyConfig(mainTarget, "CODE_SIGN_ENTITLEMENTS");
+            if (entitlementsFileName == null)
+            {
+                var bundleIdentifier = PlayerSettings.GetApplicationIdentifier(BuildTargetGroup.iOS);
+                entitlementsFileName = string.Format("{0}.entitlements",
+                    bundleIdentifier.Substring(bundleIdentifier.LastIndexOf(".") + 1));
+            }
+
+            var pbxPath = PBXProject.GetPBXProjectPath(path);
+            var capManager = new ProjectCapabilityManager(pbxPath, entitlementsFileName, "Unity-iPhone");
+            capManager.AddPushNotifications(!useReleaseAPSEnv);
+            capManager.WriteToFile();
+        }
+
+        // Get plist
+        var plistPath = path + "/Info.plist";
+        var plist = new PlistDocument();
+        plist.ReadFromString(File.ReadAllText(plistPath));
+
+        // Get root
+        var rootDict = plist.root;
+
+        var requiredVersion = new Version(8, 0);
+        bool hasMinOSVersion;
+
+        try
+        {
+            var currentVersion = new Version(PlayerSettings.iOS.targetOSVersionString);
+            hasMinOSVersion = currentVersion >= requiredVersion;
+        }
+        catch (Exception)
+        {
+            hasMinOSVersion = false;
+        }
+
+        if (!hasMinOSVersion)
+            Debug.Log("UserNotifications are only available on iOSSettings 10 and above, please make sure that you set a correct `Target minimum iOSSettings Version` in Player Settings.");
+
+        foreach (var setting in settings)
+        {
+            if (setting.Value.GetType() == typeof(bool))
+                rootDict.SetBoolean(setting.Key, (bool)setting.Value);
+            else if (setting.Value.GetType() == typeof(PresentationOption) || setting.Value.GetType() == typeof(AuthorizationOption))
+                rootDict.SetInteger(setting.Key, (int)setting.Value);
+        }
+
+        if (addPushNotificationCapability)
+        {
+            PlistElementArray currentBacgkgroundModes = (PlistElementArray)rootDict["UIBackgroundModes"];
+
+            if (currentBacgkgroundModes == null)
+                currentBacgkgroundModes = rootDict.CreateArray("UIBackgroundModes");
+
+            currentBacgkgroundModes.AddString("remote-notification");
+        }
+
+        // Write to file
+        File.WriteAllText(plistPath, plist.WriteToString());
+
+        //Get Preprocessor.h
+        var preprocessorPath = path + "/Classes/Preprocessor.h";
+        var preprocessor = File.ReadAllText(preprocessorPath);
+
+        if (needLocationFramework)
+        {
+            if (preprocessor.Contains("UNITY_USES_LOCATION"))
+                preprocessor = preprocessor.Replace("UNITY_USES_LOCATION 0", "UNITY_USES_LOCATION 1");
+        }
+
+        preprocessor = preprocessor.Replace("UNITY_USES_REMOTE_NOTIFICATIONS 0", "UNITY_USES_REMOTE_NOTIFICATIONS 1");
+        File.WriteAllText(preprocessorPath, preprocessor);
     }
 }
 #endif
