01d8d27fd49efcbef4d682a654ad039c5e0a1c09
Aurimas Cernius <aurimasc@unity3d.com>
2022-05-25 19:00:13 +0300

    Move NotificationChannelWrapper and NotificationCallback to UnityNotificationManager file

--- com.unity.mobile.notifications/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationManager.java
+++ com.unity.mobile.notifications/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationManager.java
@@ -969,3 +969,23 @@ public class UnityNotificationManager extends BroadcastReceiver {
         return mScheduledNotifications.remove(id);
     }
 }
+
+// Provide a wrapper for NotificationChannel.
+// Create this wrapper for all Android versions as NotificationChannel is only available for Android O or above.
+class NotificationChannelWrapper {
+    public String id;
+    public String name;
+    public int importance;
+    public String description;
+    public boolean enableLights;
+    public boolean enableVibration;
+    public boolean canBypassDnd;
+    public boolean canShowBadge;
+    public long[] vibrationPattern;
+    public int lockscreenVisibility;
+}
+
+// Implemented in C# to receive callback on notification show
+interface NotificationCallback {
+    void onSentNotification(Notification notification);
+}
