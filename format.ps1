8a0f502b7f25d25e23125212392e69330a831e0d
Aurimas Cernius <aurimasc@unity3d.com>
2022-09-30 16:26:01 +0300

    Remove unused formatting scripts

--- format.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Running formatter"
-perl "${Home}/unity-meta/Tools/Format/format.pl" ./com.unity.mobile.notifications/Editor ./com.unity.mobile.notifications/Runtime ./com.unity.mobile.notifications/Tests ./TestProjects/com.unity.mobile-notifications-sample
-Write-Host "Removing *.preformat.bak, *.preformat.bak.meta files"
-Get-ChildItem . -recurse -include *.preformat.bak | Remove-Item
-Get-ChildItem . -recurse -include *.preformat.bak.meta | Remove-Item
