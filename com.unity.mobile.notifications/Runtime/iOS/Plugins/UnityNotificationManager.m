b057ca82604b6447f0caf68d0c1174a33612c71d
Aurimas Cernius <aurimasc@unity3d.com>
2023-08-08 18:18:12 +0300

    Add logging about when the next calendar notification is scheduled

--- com.unity.mobile.notifications/Runtime/iOS/Plugins/UnityNotificationManager.m
+++ com.unity.mobile.notifications/Runtime/iOS/Plugins/UnityNotificationManager.m
@@ -323,6 +323,7 @@ bool validateAuthorizationStatus(UnityNotificationManager* manager)
         date.timeZone = [NSTimeZone timeZoneWithAbbreviation: @"UTC"];
 
         trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents: date repeats: data->trigger.calendar.repeats];
+        NSLog(@"Notification will show after %f s.", ((UNCalendarNotificationTrigger*)trigger).nextTriggerDate.timeIntervalSinceNow);
     }
     else if (data->triggerType == LOCATION_TRIGGER)
     {
