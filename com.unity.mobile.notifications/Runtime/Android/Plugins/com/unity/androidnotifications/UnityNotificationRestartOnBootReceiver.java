f69879d6647c05a972de908cdf22fcf63905ef1a
Aurimas Cernius <aurimasc@unity3d.com>
2023-08-16 15:16:37 +0300

    Add logging for when notifications are rescheduled/expired

--- com.unity.mobile.notifications/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationRestartOnBootReceiver.java
+++ com.unity.mobile.notifications/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationRestartOnBootReceiver.java
@@ -5,18 +5,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import static com.unity.androidnotifications.UnityNotificationManager.KEY_FIRE_TIME;
+import static com.unity.androidnotifications.UnityNotificationManager.KEY_ID;
 import static com.unity.androidnotifications.UnityNotificationManager.KEY_REPEAT_INTERVAL;
+import static com.unity.androidnotifications.UnityNotificationManager.TAG_UNITY;
 
 public class UnityNotificationRestartOnBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent received_intent) {
+        Log.d(TAG_UNITY, "Rescheduling notifications after restart");
         if (Intent.ACTION_BOOT_COMPLETED.equals(received_intent.getAction())) {
             rescheduleSavedNotifications(context);
         }
@@ -37,6 +41,8 @@ public class UnityNotificationRestartOnBootReceiver extends BroadcastReceiver {
 
             if (fireTimeDate.after(currentDate) || isRepeatable) {
                 manager.scheduleAlarmWithNotification(notificationBuilder);
+            } else {
+                Log.d(TAG_UNITY, "Notification expired, not rescheduling, ID: " + extras.getInt(KEY_ID, -1));
             }
         }
     }
