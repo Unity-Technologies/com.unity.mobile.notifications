3aa9d3b44e18fe90d4402d131c462f6f57338384
Aurimas Cernius <aurimasc@unity3d.com>
2026-07-08 14:59:29 +0300

    Remove unused import

--- com.unity.mobile.notifications/Runtime/Android/Plugins/mobilenotifications.androidlib/src/main/java/com/unity/androidnotifications/UnityNotificationManager.java
+++ com.unity.mobile.notifications/Runtime/Android/Plugins/mobilenotifications.androidlib/src/main/java/com/unity/androidnotifications/UnityNotificationManager.java
@@ -11,7 +11,6 @@ import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
